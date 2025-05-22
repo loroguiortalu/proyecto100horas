82645514f44cf88cb3170a9a706fe5bfb6998b8c
loroguiortalu <lorguiort@alu.edu.gva.es>
2025-05-22 09:29:22 +0200

    test

---
+++ Alguilaria/src/main/java/Controller/ConexionDB.java
@@ -0,0 +1,37 @@
+
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class ConexionDB {
+
+    private static Connection conexion = null;
+    
+    private ConexionDB() {}
+    
+    // función que hace que si la conexión está cerrada o es nula (como es por defecto) la crea conectandose a la base de datos pi_actfinaldb
+    public static Connection obtenerConexion() throws SQLException {
+
+        if (conexion == null || conexion.isClosed()) {
+            try {
+
+                conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/alquilariadb", "root", "");
+            } catch (SQLException e) {
+                throw new SQLException("Error: " + e.getMessage());
+            }
+        }
+        return conexion;
+        
+        
+    }
+    
+    
+    
+    // función que sirve para cerrar la conexión
+    public static void cerrarConexion() throws SQLException {
+            conexion.close();
+    }
+}
---
+++ Alguilaria/src/main/java/View/Main.java
@@ -0,0 +1,16 @@
+
+package View;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
+}
